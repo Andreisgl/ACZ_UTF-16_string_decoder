10ee6c79ccd8dbc84a9337fcadfce52c359920f8
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-09-01 01:54:08 +0300

    add routine back to a function

--- ACZ_line_swapper.py
+++ ACZ_line_swapper.py
@@ -44,40 +44,40 @@ def line_fill(curr_offset, line_size):
 
 check_files()
 
-with open(lines_file, 'rb') as lf:
-    # Divide file into important data and sections
-    
-    #SPEAKER ID
-    spkr_nol = lf.read(4) # nol = "number of lines"
-    spkr_csl = lf.read(2) # csl = "character set lenght"
-    spkr_unk1 = lf.read(10) # Unknown part
-    
-    spker_cs = lf.read(16 * int.from_bytes(spkr_csl, "little")) # Read whole cs section
+def text_splitter(initial_position):
+    with open(lines_file, 'rb') as lf:
+        # Divide file into important data and sections
+        
+        #SPEAKER ID
+        spkr_nol = lf.read(4) # nol = "number of lines"
+        spkr_csl = lf.read(2) # csl = "character set lenght"
+        spkr_unk1 = lf.read(10) # Unknown part
+        
+        spker_cs = lf.read(16 * int.from_bytes(spkr_csl, "little")) # Read whole cs section
 
-    spker_sls = lf.read(int.from_bytes(spkr_nol, "little") * 2)# sls = "string lengths"
+        spker_sls = lf.read(int.from_bytes(spkr_nol, "little") * 2)# sls = "string lengths"
 
-    aux = lf.tell() # Save position from before padding
-    aux2 = padding_skip(lf.tell(), lf) -4 # Save end of padding position
-    lf.seek(aux) # Go back to before padding
+        aux = lf.tell() # Save position from before padding
+        aux2 = padding_skip(lf.tell(), lf) -4 # Save end of padding position
+        lf.seek(aux) # Go back to before padding
 
-    spker_padding1 = lf.read(aux2 - aux) # Read padding
+        spker_padding1 = lf.read(aux2 - aux) # Read padding
 
-    spkr_so = lf.read(int.from_bytes(spkr_nol, "little") * 4) # so = "sring offset"
+        spkr_so = lf.read(int.from_bytes(spkr_nol, "little") * 4) # so = "sring offset"
 
-    spker_sd_size = 0
-    for i in range(0, int(len(spker_sls)), 2): # Iterate bytes to sum them, 2 by 2
-        b = spker_sls[i].to_bytes(1, "little") + spker_sls[i+1].to_bytes(1, "little")
-        spker_sd_size += int.from_bytes(b, byteorder = "little")  
+        spker_sd_size = 0
+        for i in range(0, int(len(spker_sls)), 2): # Iterate bytes to sum them, 2 by 2
+            b = spker_sls[i].to_bytes(1, "little") + spker_sls[i+1].to_bytes(1, "little")
+            spker_sd_size += int.from_bytes(b, byteorder = "little")  
 
-    spkr_sd = lf.read(spker_sd_size *2) # sd = "string data"
-    
-    padding_length = line_fill(lf.tell(), 16) # How many bytes to add as end-of-line padding
-    spkr_sd += lf.read(padding_length)
-    
-    ## Skip the bitmap sheet and palette data for SPEAKER_ID strings and jump to where the mission-relevant string data is located
-    lf.seek((int.from_bytes(spkr_csl, "little") * 512) + 64 + lf.tell(), 0)
+        spkr_sd = lf.read(spker_sd_size *2) # sd = "string data"
+        
+        padding_length = line_fill(lf.tell(), 16) # How many bytes to add as end-of-line padding
+        spkr_sd += lf.read(padding_length)
+        
+        ## Skip the bitmap sheet and palette data for SPEAKER_ID strings and jump to where the mission-relevant string data is located
+        lf.seek((int.from_bytes(spkr_csl, "little") * 512) + 64 + lf.tell(), 0)
 
-    #VOICE LINES
 
 
 
