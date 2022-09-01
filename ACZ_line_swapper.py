fba156042bbf8ce5ce232a5b7680b78ed19c6660
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-09-01 23:44:47 +0300

    progressing

--- ACZ_line_swapper.py
+++ ACZ_line_swapper.py
@@ -12,6 +12,34 @@ from sys import byteorder
 
 basedir = os.getcwd()
 folders_list = []
+current_folder = ""
+
+file_list = []
+short_file_list = [] # Filenames without number prefix and extension
+path_file_list = [] # Filenames with path in folder
+
+sil = [] # Short fo "section_index_list" Index of each section in the previous lists
+
+# List of file names needed for the job
+speaker_nol = "speaker_nol"
+speaker_csl = "speaker_csl"
+speaker_unknown = "speaker_unknown"
+speaker_cs = "speaker_cs"
+speaker_sls = "speaker_sls"
+speaker_padd1 = "speaker_padd1"
+speaker_so = "speaker_so"
+speaker_sd = "speaker_sd"
+interstitial1_interstitial1 = "interstitial1_interstitial1"
+#
+radio_nol = "radio_nol"
+radio_csl = "radio_csl"
+radio_unknown = "radio_unknown"
+radio_cs = "radio_cs"
+radio_sls = "radio_sls"
+radio_padd1 = "radio_padd1"
+radio_so = "radio_so"
+radio_sd = "radio_sd"
+interstitial2_interstitial2 = "interstitial2_interstitial2"
 
 # Prompts user to choose folder from folders_list.
 # Returns index of folder to be worked on.
@@ -52,4 +80,49 @@ def choose_working_folder():
     return(choice)
     print()
 
-choose_working_folder()
+def check_files_in_folder():
+    file_list = os.listdir(current_folder)
+    try:
+        for i in range(len(file_list)):
+            while (os.path.isdir(basedir + "/" + file_list[i])):
+                file_list.pop(i)
+    except IndexError:
+        pass
+
+    for i in range(len(file_list)):
+        short_file_list.append(file_list[i][5:-4])
+    for i in range(len(file_list)):
+        path_file_list.append(current_folder + "/" + file_list[i])
+    
+    sil.append( short_file_list.index(speaker_nol))
+    sil.append( short_file_list.index(speaker_csl))
+    sil.append( short_file_list.index(speaker_unknown))
+    sil.append( short_file_list.index(speaker_cs))
+    sil.append( short_file_list.index(speaker_sls))
+    sil.append( short_file_list.index(speaker_padd1))
+    sil.append( short_file_list.index(speaker_so))
+    sil.append( short_file_list.index(speaker_sd))
+    sil.append( short_file_list.index(interstitial1_interstitial1))
+    #
+    sil.append( short_file_list.index(radio_nol))
+    sil.append( short_file_list.index(radio_csl))
+    sil.append( short_file_list.index(radio_unknown))
+    sil.append( short_file_list.index(radio_cs))
+    sil.append( short_file_list.index(radio_sls))
+    sil.append( short_file_list.index(radio_padd1))
+    sil.append( short_file_list.index(radio_so))
+    sil.append( short_file_list.index(radio_sd))
+    sil.append( short_file_list.index(interstitial2_interstitial2))
+    
+    
+def manipulate_text(nol, csl, unk, cs, sls, padd1, so, sd):
+    
+    print()
+
+current_folder = choose_working_folder()
+current_folder = "./" + folders_list[current_folder]
+check_files_in_folder()
+
+manipulate_text(sil[0], sil[1], sil[2], sil[3], sil[4], sil[5], sil[6], sil[7]) # Test for speaker stuff
+
+print("end")
