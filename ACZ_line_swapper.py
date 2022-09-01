fc65eb45cf0404ac88837a0b4026ef23c7f534b0
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-09-01 20:25:33 +0300

    create line swapper

---
+++ ACZ_line_swapper.py
@@ -0,0 +1,5 @@
+## ACE COMBAT ZERO line swapper by Andrei Segal (Andreisgl @ GitHub, SegalAndrei @ Twitter)
+## Based on ACE COMBAT ZERO string decoder by death_the_d0g (deththed0g @ GitHub, death_the_d0g @ Twitter)
+## =========================================================================
+## Allows for changing of mission radio lines and names. Intended for translation.
+## (Warranty void if used for memes)
