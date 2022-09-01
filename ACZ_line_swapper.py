2378b0bf85292a2bfcd5ef4cada3105277c393b7
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-09-01 14:51:56 +0300

    remove reassembling test script

--- ACZ_line_swapper.py
+++ ACZ_line_swapper.py
@@ -103,20 +103,6 @@ with open(lines_file, "rb") as lf:
 
 
 ######
-test_file = "ddd.dbg"
-with open(test_file, "wb") as f:
-    # Speaker
-    for i in range(len(speaker_data_list) -1):
-        f.write(speaker_data_list[i])
-    
-    # Interstitial1
-    f.write(interstitial1)
 
-    # Voice lines
-    for i in range(len(voice_data_list) -1):
-        f.write(voice_data_list[i])
-    
-    # Interstitial2
-    f.write(interstitial2)
 
 print("end")
